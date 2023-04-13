6b51c5b7f044fda2aced02a61015084937df9be0
Asef Islam <asef@cs.stanford.edu>
2023-04-12 18:41:28 -0700

    Error handling in JavaGet

---
+++ src/main/java/org/metadatacenter/reporting/NumberOfArtifacts.java
@@ -0,0 +1,74 @@
+import java.time.LocalDateTime;
+import java.util.List;
+
+/** Class to generate reports for numbers of artifacts
+ *
+ */
+public class NumberOfArtifacts {
+
+  /**
+   * Returns a list of number of templates per user sorted in decreasing order
+   * of the number of templates the user has produced, along with a timestamp.
+   * @param  folder the directory to include in analysis
+   * @return List<Integer> list templatesPerUser;
+   */
+  public List<Integer> getTemplatesPerUser(String folder) {
+    return null;
+  }
+
+  /**
+   * Returns a list of number of templates per user sorted in decreasing order
+   * of the number of templates the user has produced, along with a timestamp.
+   * @param  folder the directory to include in analysis
+   * @return List<Integer> list templatesPerUser;
+   */
+  public List<Integer> getElementsPerUser(String folder) {
+    return null;
+  }
+
+  /**
+   * Returns a list of number of templates per user sorted in decreasing order
+   * of the number of templates the user has produced, along with a timestamp.
+   * @param  folder the directory to include in analysis
+   * @return List<Integer> list templatesPerUser;
+   */
+  public List<Integer> getFieldsPerUser(String folder) {
+    return null;
+  }
+
+  /**
+   * Returns a list of number of templates per user sorted in decreasing order
+   * of the number of templates the user has produced, along with a timestamp.
+   * @param  folder the directory to include in analysis
+   * @return List<Integer> list templatesPerUser;
+   */
+  public List<Integer> getInstancesPerUser(String folder) {
+    return null;
+  }
+
+  /**
+   * Returns a list of number of templates per user sorted in decreasing order
+   * of the number of templates the user has produced, along with a timestamp.
+   * @param  folder the directory to include in analysis
+   * @return List<Integer> list templatesPerUser;
+   */
+  public List<Integer> getArtifactsPerUser(String folder) {
+    return null;
+  }
+
+
+
+
+  /**
+   * Main method
+   * @param  args
+   * @return none
+   */
+  public static void main (String[] args) {
+    //return null;
+    //folder = args[1];
+    //JavaGet(folder);
+
+  }
+
+}
