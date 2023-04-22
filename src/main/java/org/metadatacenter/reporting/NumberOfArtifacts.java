5196734dadb9ac6b5b003a10eb556b62e1ea961c
Asef Islam <asef@cs.stanford.edu>
2023-04-21 20:22:03 -0700

    Refactored GetResourcesPerUser to be more flexible and accept resource type parameter

--- src/main/java/org/metadatacenter/reporting/NumberOfArtifacts.java
+++ src/main/java/org/metadatacenter/reporting/NumberOfArtifacts.java
@@ -1,8 +1,7 @@
 package org.metadatacenter.reporting;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.List;
+import java.util.*;
+
 import org.metadatacenter.reporting.models.PathInfo;
 import org.metadatacenter.reporting.GetRequest;
 import org.metadatacenter.reporting.models.Root;
@@ -18,18 +17,37 @@ public class NumberOfArtifacts {
    * Returns a list of number of templates per user sorted in decreasing order
    * of the number of templates the user has produced, along with a timestamp.
    * @param  folder the directory to include in analysis
+   * @param resourceType The type of resource of interest,
+   *                     can be "template", "element", "field", "instance", or "artifact"
    * @return List<Integer> list templatesPerUser;
    */
-  public static List<Integer> getTemplatesPerUser(String folder) {
+  public static List<Integer> getResourcesPerUser(String folder, String resourceType) {
     List<Root> folders = Get(folder, "folder");
     Hashtable<String, String> idsAndAuthors = new Hashtable<String, String>();
-    for (Root f: folders) {
-      for (PathInfo p : f.pathInfo) {
-        idsAndAuthors.put(p.id, p.pav_createdBy);
+    for (Root f: folders) {        // loop through folders
+      for (PathInfo p : f.pathInfo) {  // loop through content within folders
+        if (p.resourceType == resourceType) {     // if resource is the type of interest
+          idsAndAuthors.put(p.id, p.pav_createdBy);  // add to dictionary
+        }
       }
     }
-    List<Integer> authorCounts = new ArrayList<Integer>();
-    return null;
+    Hashtable<String, Integer> authorCounts = new Hashtable<String, Integer>(); // how many times each author appears
+    for (Map.Entry<String,String> entry: idsAndAuthors.entrySet()) {   // loop through resources
+      String id = entry.getKey();
+      String author = entry.getValue();
+      if (authorCounts.containsKey(author)) {   // if this author is already in author counts dictionary
+        authorCounts.put(author, authorCounts.get(author) + 1); // increment count for this author
+      }
+      else {
+        authorCounts.put(author, 1); // otherwise, put a 1 as our first count for this author
+      }
+    }
+    List<Integer> counts = new ArrayList<Integer>(); // list of counts only
+    for (Map.Entry<String,Integer> entry: authorCounts.entrySet()) {     // loop through resources
+      counts.add(entry.getValue());  // put the counts into a list
+    }
+    Collections.sort(counts); // sort the array
+    return counts;
   }
 
   /**
@@ -79,7 +97,7 @@ public class NumberOfArtifacts {
    */
   public static void main (String[] args) {
     String folder = args[0];
-    getTemplatesPerUser(folder);
+    getResourcesPerUser(folder, "template");
   }
 
 }
